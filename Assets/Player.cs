8e13c7be7fd964ae0d7c3267250931e3d95c7bb6
GenisLopez5 <misterproxx@gmail.com>
2026-01-30 17:40:19 +0100

    added vida

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int vida = 100;
     }
 
     // Update is called once per frame
